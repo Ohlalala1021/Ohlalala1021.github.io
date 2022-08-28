412da2c88c514b887c50679ccb7336053227cc72
Oh la la la1021 <camear.xiaoy@myxiaojin.cn>
2022-08-28 17:36:17 +0800

    update

---
+++ happydoggy/.vuepress/.temp/pages/aboutme.html.js
@@ -0,0 +1,14 @@
+export const data = JSON.parse("{\"key\":\"v-03de6b8d\",\"path\":\"/aboutme.html\",\"title\":\"关于我……(^_^)\",\"lang\":\"zh-CN\",\"frontmatter\":{\"home\":true,\"icon\":\"home\",\"title\":\"关于我……(^_^)\",\"heroImage\":\"/aboutme.webp\",\"heroText\":\"关于Mr.11.爱宠人\",\"tagline\":\"lalalalalalalala\",\"summary\":\"乘着飞机， 乘着满满的期待， 起飞。 在云之路上穿行， 雪白的小羊，歌唱。 是向往的旋律， 轻盈而悠扬。 是谁，撒下了霞光？ 星星点点的， 落在了棉花糖上。 降落。 回望祖国山河壮美， 城市高楼林立， 而我的路，始于足下。 踏着石子， 踏着坚实的脚步，进发。 在布满荆棘的路上前行。 凹凸的石子，欢笑， 是鼓励的掌声， 沉稳而坚实。 是谁，磨平了棱角，磨出了心\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://vuepress-theme-hope-v2-demo.mrhope.site/aboutme.html\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"oh-la!\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"关于我……(^_^)\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"website\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}]]},\"excerpt\":\"\",\"headers\":[],\"readingTime\":{\"minutes\":0.58,\"words\":173},\"filePathRelative\":\"aboutme.md\"}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
