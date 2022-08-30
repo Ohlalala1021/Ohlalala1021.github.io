02e508eda202e979d935adcfd3b126ad6ce4fb5e
Oh la la la1021 <camear.xiaoy@myxiaojin.cn>
2022-08-30 21:12:59 +0800

    update

--- happydoggy/.vuepress/.temp/blog/type.js
+++ happydoggy/.vuepress/.temp/blog/type.js
@@ -1,4 +1,4 @@
-export const typeMap = {"article":{"/":{"path":"/article/","keys":["v-0e503981"]}},"encrypted":{"/":{"path":"/encrypted/","keys":[]}},"slide":{"/":{"path":"/slide/","keys":["v-0e503981"]}},"star":{"/":{"path":"/star/","keys":[]}},"timeline":{"/":{"path":"/timeline/","keys":[]}}}
+export const typeMap = {"article":{"/":{"path":"/article/","keys":["v-73e015f7"]}},"encrypted":{"/":{"path":"/encrypted/","keys":[]}},"slide":{"/":{"path":"/slide/","keys":[]}},"star":{"/":{"path":"/star/","keys":[]}},"timeline":{"/":{"path":"/timeline/","keys":["v-73e015f7"]}}}
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept()
--- happydoggy/.vuepress/.temp/internal/pagesRoutes.js
+++ happydoggy/.vuepress/.temp/internal/pagesRoutes.js
@@ -1,6 +1,7 @@
 export const pagesRoutes = [
   ["v-8daa1a0e","/",{"title":"lalalala","icon":"home","type":"home","readingTime":{"minutes":0.17,"words":52},"excerpt":""},["/index.html","/README.md"]],
   ["v-03de6b8d","/aboutme.html",{"title":"关于我……(^_^)","icon":"home","type":"home","readingTime":{"minutes":0.58,"words":173},"excerpt":"乘着飞机， 乘着满满的期待， 起飞。 在云之路上穿行， 雪白的小羊，歌唱。 是向往的旋律， 轻盈而悠扬。 是谁，撒下了霞光？ 星星点点的， 落在了棉花糖上。 降落。 回望祖国山河壮美， 城市高楼林立， 而我的路，始于足下。 踏着石子， 踏着坚实的脚步，进发。 在布满荆棘的路上前行。 凹凸的石子，欢笑， 是鼓励的掌声， 沉稳而坚实。 是谁，磨平了棱角，磨出了心"},["/aboutme","/aboutme.md"]],
+  ["v-73e015f7","/1/thatbuttifulplace.html",{"title":"暑假小文——那片美丽的风景-重庆","type":"article","readingTime":{"minutes":1.99,"words":597},"excerpt":"那片美丽的风景-重庆 2022.7.26 如果杭州是东坡肉，又软又糯，那么重庆就是辣子鸡，又香又酥；如果杭州的山是秀美的，那么重庆的山就是壮硕的；如果杭州的湖是优雅平静的，那么重庆的江就是交融奔腾的；如果杭州的街头是文艺的，那么重庆的街头就是烟火气的；如果你问我中国最具烟火气的地方是哪里，我一定会毫不犹豫地回答你——重庆。 ​ 重庆的烟火气是本地人的豪爽。重","author":"Mr.16.爱宠人","date":"2022-08-30T20:36:00.000Z","localizedDate":"2022年8月31日","isOriginal":true},["/1/thatbuttifulplace","/1/thatbuttifulplace.md"]],
   ["v-3706649a","/404.html",{"title":"","type":"page","readingTime":{"minutes":0,"words":0},"excerpt":""},["/404"]],
   ["v-5bc93818","/category/",{"title":"分类","type":"page","readingTime":{"minutes":0,"words":0},"excerpt":""},["/category/index.html"]],
   ["v-744d024e","/tag/",{"title":"标签","type":"page","readingTime":{"minutes":0,"words":0},"excerpt":""},["/tag/index.html"]],
@@ -9,5 +10,4 @@ export const pagesRoutes = [
   ["v-d804e652","/slide/",{"title":"幻灯片","type":"page","readingTime":{"minutes":0,"words":0},"excerpt":""},["/slide/index.html"]],
   ["v-154dc4c4","/star/",{"title":"收藏","type":"page","readingTime":{"minutes":0,"words":0},"excerpt":""},["/star/index.html"]],
   ["v-01560935","/timeline/",{"title":"时间轴","type":"page","readingTime":{"minutes":0,"words":0},"excerpt":""},["/timeline/index.html"]],
-  ["v-73e015f7","/1/thatbuttifulplace.html",{"title":"暑假小文——那片美丽的风景-重庆","type":"article","readingTime":{"minutes":1.99,"words":597},"excerpt":"那片美丽的风景-重庆 2022.7.26 如果杭州是东坡肉，又软又糯，那么重庆就是辣子鸡，又香又酥；如果杭州的山是秀美的，那么重庆的山就是壮硕的；如果杭州的湖是优雅平静的，那么重庆的江就是交融奔腾的；如果杭州的街头是文艺的，那么重庆的街头就是烟火气的；如果你问我中国最具烟火气的地方是哪里，我一定会毫不犹豫地回答你——重庆。 ​ 重庆的烟火气是本地人的豪爽。重","author":"Mr.16.爱宠人","date":"2022-08-30T20:36:00.000Z","localizedDate":"2022年8月31日","isOriginal":true},["/1/thatbuttifulplace","/1/thatbuttifulplace.md"]],
 ]
