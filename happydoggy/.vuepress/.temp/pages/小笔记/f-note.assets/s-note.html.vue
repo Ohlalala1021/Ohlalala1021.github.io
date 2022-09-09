b400f17e78967e17c1e9e1d4bd83fa9d7e89b646
Oh la la la1021 <camear.xiaoy@myxiaojin.cn>
2022-09-09 21:02:17 +0800

    update

---
+++ happydoggy/.vuepress/.temp/pages/小笔记/f-note.assets/s-note.html.vue
@@ -0,0 +1,10 @@
+<template><div><p>1、加密🔐</p>
+<p>如何加密中文文件：[encodeURI(&quot;/链接/文件名/&quot;)]:&quot;密码&quot;</p>
+<p>2、修改菜单栏</p>
+<p>text：名字</p>
+<p>link：链接</p>
+<p>Icon:图标</p>
+<p>Children:子菜单</p>
+<p>用逗号隔开</p>
+<p text:菜单栏名,link:文件夹名文件名=""></p>
+</div></template>
---
+++ happydoggy/.vuepress/.temp/pages/小随笔/2022-9/9-7.html.js
@@ -0,0 +1,14 @@
+export const data = JSON.parse("{\"key\":\"v-00a3814e\",\"path\":\"/%E5%B0%8F%E9%9A%8F%E7%AC%94/2022-9/9-7.html\",\"title\":\"”作业榜”榜首\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"”作业榜”榜首\",\"date\":\"2022-09-06T20:24:07.000Z\",\"author\":\"Mr.11.爱宠人\",\"isOriginal\":true,\"category\":[\"小随笔\"],\"tag\":[\"随笔\"],\"icon\":\"blog\",\"summary\":\"9.7 今天估计是我最高效的一天，因为我竟然上了作业榜第一名！也就是说我做作业清作业的效率比兰泽青、李子澄、陈孙千雨这些学霸还快！这让我感到十分的自豪，毕竟段时间内超越学霸挺难的。我不仅快，质量还很高，小练笔是A+，课堂作业本也是A+，还花了课余时间把语文学习导图和论文写作导图做了。明天，我要更加努力，争取把数学也做完做对，嘻嘻嘻～\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://www.thomasxiao.com/%E5%B0%8F%E9%9A%8F%E7%AC%94/2022-9/9-7.html\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"oh-la!\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"”作业榜”榜首\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}],[\"meta\",{\"property\":\"article:author\",\"content\":\"Mr.11.爱宠人\"}],[\"meta\",{\"property\":\"article:tag\",\"content\":\"随笔\"}],[\"meta\",{\"property\":\"article:published_time\",\"content\":\"2022-09-06T20:24:07.000Z\"}]]},\"excerpt\":\"\",\"headers\":[],\"readingTime\":{\"minutes\":0.58,\"words\":175},\"filePathRelative\":\"小随笔/2022-9/9-7.md\",\"localizedDate\":\"2022年9月7日\"}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
