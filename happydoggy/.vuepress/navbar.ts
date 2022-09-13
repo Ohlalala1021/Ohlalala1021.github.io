24af4f40e3e41170885a42873ff59065ad18d8d1
Oh la la la1021 <camear.xiaoy@myxiaojin.cn>
2022-09-13 20:11:19 +0800

    update

--- happydoggy/.vuepress/navbar.ts
+++ happydoggy/.vuepress/navbar.ts
@@ -13,9 +13,14 @@ export default navbar([
     {
       text:"书写美好",
       children: [
-        { text: "学而时习之", icon: "edit", link: "/小导图/thedynastyandtrustofthepeople"},
+        { text: "我的狗狗", icon: "edit", link: "/小美文/cookiemydog"},
+        { text: "四季老和山", icon: "edit", link: "/小美文/hilllaoheshan"},
+        { text: "谈彭州山洪", icon: "edit", link: "/小美文/pengzhoulandslide"},
+        { text: "那片美丽的风景", icon: "edit", link: "/小美文/thatbuttifulplace"},
+        { text: "那一刻我长大了", icon: "edit", link: "/小美文/thatmomentivegrown"},
+        { text: "卜算子咏春",icon: "edit", link: "/小美文/thespringsong"},
     ],
     }
         ],
-      },
+  },
 ])
