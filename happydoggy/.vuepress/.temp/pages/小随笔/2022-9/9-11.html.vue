a0d507426e1ddcb2447a256e7d46d091125b36fa
Oh la la la1021 <camear.xiaoy@myxiaojin.cn>
2022-09-13 21:47:21 +0800

    update

--- happydoggy/.vuepress/.temp/pages/小随笔/2022-9/9-11.html.vue
+++ happydoggy/.vuepress/.temp/pages/小随笔/2022-9/9-11.html.vue
@@ -1,8 +1,8 @@
 <template><div><p>9.11</p>
-<p><img src="@source/小随笔/2022-9/9-11.assets/image-20220913211352241.png" alt="image-20220913211352241"></p>
-<p><img src="@source/小随笔/2022-9/9-11.assets/image-20220913211502568.png" alt="image-20220913211502568"></p>
-<p><img src="@source/小随笔/2022-9/9-11.assets/image-20220913211542892.png" alt="image-20220913211542892"></p>
-<p><img src="@source/小随笔/2022-9/9-11.assets/image-20220913211639263.png" alt="image-20220913211639263"></p>
+<p><img src="@source/小随笔/2022-9/9-11.assets/image-20220913214500263.png" alt="image-20220913214500263"></p>
+<p><img src="@source/小随笔/2022-9/9-11.assets/image-20220913214531518.png" alt="image-20220913214531518"></p>
+<p><img src="@source/小随笔/2022-9/9-11.assets/image-20220913214554257.png" alt="image-20220913214554257"></p>
+<p><img src="@source/小随笔/2022-9/9-11.assets/image-20220913214616208.png" alt="image-20220913214616208"></p>
 <p>今天去摘板栗🌰，我像一只开心栗鼠一样，在板栗山上到处找板栗。可惜它们都还没熟，开心栗鼠变成了伤心栗鼠……</p>
 <p>This Mid-autumn moon festival I have three interesting things to share with you.</p>
 <p>First of all, Ruby and I went to the Chestnut hill to pick chestnuts, we discussed our roles in this activity on our way there, I was responsible for shaking the chestnuts from the tree, and Ruby was responsible for picking them up from the ground. Unfortunately, we were about three weeks early and the chestnuts were not ready to pick, because there were still green and growing on the trees. Still we had a go, and picked some green ones, but Ruby’s grandpa said there were none can be eaten, what a pity! But we still had fun and agreed we are going to come back when the nuts are ready.</p>
