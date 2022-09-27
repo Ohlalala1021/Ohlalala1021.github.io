8ed9cc5ee77bafd27fdadb6cc55d3aa8b29c5227
Oh la la la1021 <camear.xiaoy@myxiaojin.cn>
2022-09-27 20:39:12 +0800

    update

--- happydoggy/.vuepress/.temp/internal/pagesData.js
+++ happydoggy/.vuepress/.temp/internal/pagesData.js
@@ -5,14 +5,6 @@ export const pagesData = {
   "v-03de6b8d": () => import(/* webpackChunkName: "v-03de6b8d" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/aboutme.html.js").then(({ data }) => data),
   // path: /%E5%B0%8F%E7%AC%94%E8%AE%B0/f-note.html
   "v-135ba9df": () => import(/* webpackChunkName: "v-135ba9df" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/小笔记/f-note.html.js").then(({ data }) => data),
-  // path: /%E5%B0%8F%E5%AF%BC%E5%9B%BE/thedynastyandtrustofthepeople.html
-  "v-315fef65": () => import(/* webpackChunkName: "v-315fef65" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/小导图/thedynastyandtrustofthepeople.html.js").then(({ data }) => data),
-  // path: /%E5%B0%8F%E7%9B%AE%E6%A0%87/5thgradeterm1.html
-  "v-61ed2ca1": () => import(/* webpackChunkName: "v-61ed2ca1" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/小目标/5thgradeterm1.html.js").then(({ data }) => data),
-  // path: /%E5%B0%8F%E7%9B%AE%E6%A0%87/summerholiday2022.html
-  "v-0014b6d1": () => import(/* webpackChunkName: "v-0014b6d1" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/小目标/summerholiday2022.html.js").then(({ data }) => data),
-  // path: /%E5%B0%8F%E8%AE%BA%E6%96%87/thedynastyandtrustofthepeople.html
-  "v-084b679e": () => import(/* webpackChunkName: "v-084b679e" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/小论文/thedynastyandtrustofthepeople.html.js").then(({ data }) => data),
   // path: /%E5%B0%8F%E7%BE%8E%E6%96%87/cookiemydog.html
   "v-44a42a29": () => import(/* webpackChunkName: "v-44a42a29" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/小美文/cookiemydog.html.js").then(({ data }) => data),
   // path: /%E5%B0%8F%E7%BE%8E%E6%96%87/hilllaoheshan.html
@@ -25,6 +17,14 @@ export const pagesData = {
   "v-71c3455a": () => import(/* webpackChunkName: "v-71c3455a" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/小美文/thatmomentivegrown.html.js").then(({ data }) => data),
   // path: /%E5%B0%8F%E7%BE%8E%E6%96%87/thespringsong.html
   "v-2aef503c": () => import(/* webpackChunkName: "v-2aef503c" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/小美文/thespringsong.html.js").then(({ data }) => data),
+  // path: /%E5%B0%8F%E5%AF%BC%E5%9B%BE/thedynastyandtrustofthepeople.html
+  "v-315fef65": () => import(/* webpackChunkName: "v-315fef65" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/小导图/thedynastyandtrustofthepeople.html.js").then(({ data }) => data),
+  // path: /%E5%B0%8F%E8%AE%BA%E6%96%87/thedynastyandtrustofthepeople.html
+  "v-084b679e": () => import(/* webpackChunkName: "v-084b679e" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/小论文/thedynastyandtrustofthepeople.html.js").then(({ data }) => data),
+  // path: /%E5%B0%8F%E7%9B%AE%E6%A0%87/5thgradeterm1.html
+  "v-61ed2ca1": () => import(/* webpackChunkName: "v-61ed2ca1" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/小目标/5thgradeterm1.html.js").then(({ data }) => data),
+  // path: /%E5%B0%8F%E7%9B%AE%E6%A0%87/summerholiday2022.html
+  "v-0014b6d1": () => import(/* webpackChunkName: "v-0014b6d1" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/小目标/summerholiday2022.html.js").then(({ data }) => data),
   // path: /%E5%B0%8F%E7%AC%94%E8%AE%B0/f-note.assets/s-note.html
   "v-05997c16": () => import(/* webpackChunkName: "v-05997c16" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/小笔记/f-note.assets/s-note.html.js").then(({ data }) => data),
   // path: /%E5%B0%8F%E9%9A%8F%E7%AC%94/2022-8/8-31.html
@@ -69,22 +69,22 @@ export const pagesData = {
   "v-47b840c0": () => import(/* webpackChunkName: "v-47b840c0" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/category/小笔记/index.html.js").then(({ data }) => data),
   // path: /tag/note/
   "v-28e08cef": () => import(/* webpackChunkName: "v-28e08cef" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/tag/note/index.html.js").then(({ data }) => data),
-  // path: /category/%E5%B0%8F%E5%AF%BC%E5%9B%BE/
-  "v-cccb8ad8": () => import(/* webpackChunkName: "v-cccb8ad8" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/category/小导图/index.html.js").then(({ data }) => data),
-  // path: /tag/%E5%AF%BC%E5%9B%BE/
-  "v-309fdd43": () => import(/* webpackChunkName: "v-309fdd43" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/tag/导图/index.html.js").then(({ data }) => data),
-  // path: /category/%E5%B0%8F%E7%9B%AE%E6%A0%87/
-  "v-7b3d61be": () => import(/* webpackChunkName: "v-7b3d61be" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/category/小目标/index.html.js").then(({ data }) => data),
-  // path: /tag/%E7%9B%AE%E6%A0%87/
-  "v-1243046d": () => import(/* webpackChunkName: "v-1243046d" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/tag/目标/index.html.js").then(({ data }) => data),
-  // path: /category/%E5%B0%8F%E8%AE%BA%E6%96%87/
-  "v-31aedde6": () => import(/* webpackChunkName: "v-31aedde6" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/category/小论文/index.html.js").then(({ data }) => data),
-  // path: /tag/%E8%AE%BA%E6%96%87/
-  "v-7e2e33bc": () => import(/* webpackChunkName: "v-7e2e33bc" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/tag/论文/index.html.js").then(({ data }) => data),
   // path: /category/%E5%B0%8F%E7%BE%8E%E6%96%87/
   "v-12071aca": () => import(/* webpackChunkName: "v-12071aca" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/category/小美文/index.html.js").then(({ data }) => data),
   // path: /tag/%E7%BE%8E%E6%96%87/
   "v-e3fbd56c": () => import(/* webpackChunkName: "v-e3fbd56c" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/tag/美文/index.html.js").then(({ data }) => data),
+  // path: /category/%E5%B0%8F%E5%AF%BC%E5%9B%BE/
+  "v-cccb8ad8": () => import(/* webpackChunkName: "v-cccb8ad8" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/category/小导图/index.html.js").then(({ data }) => data),
+  // path: /tag/%E5%AF%BC%E5%9B%BE/
+  "v-309fdd43": () => import(/* webpackChunkName: "v-309fdd43" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/tag/导图/index.html.js").then(({ data }) => data),
+  // path: /category/%E5%B0%8F%E8%AE%BA%E6%96%87/
+  "v-31aedde6": () => import(/* webpackChunkName: "v-31aedde6" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/category/小论文/index.html.js").then(({ data }) => data),
+  // path: /tag/%E8%AE%BA%E6%96%87/
+  "v-7e2e33bc": () => import(/* webpackChunkName: "v-7e2e33bc" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/tag/论文/index.html.js").then(({ data }) => data),
+  // path: /category/%E5%B0%8F%E7%9B%AE%E6%A0%87/
+  "v-7b3d61be": () => import(/* webpackChunkName: "v-7b3d61be" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/category/小目标/index.html.js").then(({ data }) => data),
+  // path: /tag/%E7%9B%AE%E6%A0%87/
+  "v-1243046d": () => import(/* webpackChunkName: "v-1243046d" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/tag/目标/index.html.js").then(({ data }) => data),
   // path: /category/%E5%B0%8F%E9%9A%8F%E7%AC%94/
   "v-1973b53e": () => import(/* webpackChunkName: "v-1973b53e" */"/Users/thomas/happydoggy/Ohlalala1021.github.io/happydoggy/.vuepress/.temp/pages/category/小随笔/index.html.js").then(({ data }) => data),
   // path: /tag/%E9%9A%8F%E7%AC%94/
--- happydoggy/.vuepress/config.ts
+++ happydoggy/.vuepress/config.ts
@@ -1,11 +1,20 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme";
-
+import { searchPlugin } from "@vuepress/plugin-search";
 export default defineUserConfig({
   lang: "zh-CN",
   title: "oh-la!",
   description: "vuepress-theme-hope 的演示",
 
   base: "/",
+  plugins:[
+    searchPlugin({
+      locales:{
+        "/":{
+          placeholder:"搜索",
 
+        },
+       },
+    }),
+   ],
   theme,});
